134da1c4134b19802c813aa6bc845d3477acd382
simseulnyang <happysseul627@gmail.com>
2024-01-31 20:25:42 +0900

    🎨 Style:access 변수명 access_token으로 변경

--- src/js/logout.js
+++ src/js/logout.js
@@ -4,8 +4,7 @@ export const logout = () => {
     const logout_button = document.getElementById('loginout');
 
     logout_button.addEventListener('click', async function() {
-        localStorage.removeItem("access");
-        localStorage.removeItem("refresh");
+        localStorage.removeItem("access_token");
         localStorage.removeItem("payload");
         alert('로그아웃되었습니다.');
         location.href="/index.html";
